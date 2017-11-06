9a50e5ecd8957d912403f730c534ea620cd16d33
Najah Izquierdo <najah.izquierdo@yahoo.com>
2017-11-06 11:41:02 -0500

    Fix User Auth error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+
+   def current_user
+     @current_user ||= User.find_by(id: session[:user_id])
+   end
+
+   def login?
+     !!current_user
+   end
+
+   def require_user
+     redirect_to '/login' unless login?
+   end
 end
