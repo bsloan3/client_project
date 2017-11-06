76fbb32ab1f0a22edad99df24b5c035794216d4e
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-11-06 15:09:12 -0500

    Add scope validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, :email, presence: true, uniqueness:true
+  
 
   has_many :ratings, dependent: :destroy
   has_many :recipes, class_name: "Recipe", foreign_key: "user_id", dependent: :destroy
