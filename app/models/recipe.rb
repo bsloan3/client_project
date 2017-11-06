21eb6d8fcdb3233d8f3aba06cc4dd5022f37b5fa
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-11-06 15:01:08 -0500

    add dependent destroy to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
 
-  has_many :ingredients
-  has_many :ratings
+  has_many :ingredients, dependent: :destroy
+  has_many :ratings, dependent: :destroy
 
 
   validates :name, :difficulty_level, :prep_time, :directions, :user, :category, presence: :true
