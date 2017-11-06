9f3c6f4b29fe2a6afc408affde49106543e40317
Thomas Rafferty <thomasjrafferty@gmail.com>
2017-11-06 12:20:16 -0500

    Work on create recipe routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+#Mentor taught Thomas this, look into functionality
+# before_action :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
