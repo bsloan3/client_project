2061386f9f0c19308b57d24085f5d96ae07ccf62
Najah Izquierdo <najah.izquierdo@yahoo.com>
2017-11-06 19:15:37 -0500

    Add styling to search bar

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
   get 'users/:id', to: 'users#show'
+  get '/ratings', to: 'ratings#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
