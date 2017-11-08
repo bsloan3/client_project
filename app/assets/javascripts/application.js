0941e755de68a84e9c3fa386d466f0ba94e1535b
Najah Izquierdo <najah.izquierdo@yahoo.com>
2017-11-07 23:52:13 -0500

    Add nav bar styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,13 @@ jQuery(document).ready(function() {
   });
 
 });
+
+$(function(){
+$("#addClass").click(function () {
+        $('#qnimate').addClass('popup-box-on');
+          });
+
+          $("#removeClass").click(function () {
+        $('#qnimate').removeClass('popup-box-on');
+          });
+})
